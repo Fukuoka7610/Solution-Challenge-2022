185870fe83b85be730cb138e3aad88ecc5b640b3
Hiroto <hiro.f7610@gmail.com>
2022-03-05 19:31:13 +0900

    Map implementation

--- React/web/src/Map/Map.js
+++ React/web/src/Map/Map.js
@@ -1,8 +1,30 @@
+import { GoogleMap, LoadScript } from "@react-google-maps/api";
+
 import React from 'react'
+
+const containerStyle = {
+  width: '100%',
+  height: '90vh',
+};
+
+const center = {
+  lat: 35.69575,
+  lng: 139.77521,
+};
 
 const Map = () => {
   return (
-    <div>Map Page</div>
+    <div>
+      Map Page
+      <LoadScript googleMapsApiKey='AIzaSyCA54hI6blCLXItNTOXpx8AImUykaMyMTg'>
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={center}
+          zoom={17}
+        ></GoogleMap>
+      </LoadScript>
+    </div>
+
   )
 }
 
